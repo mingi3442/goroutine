94717045b93c4685070b7605555daab2eefed858
Lee MinGi <mingi3442@gmail.com>
2024-04-05 00:11:02 +0900

    Limiting the number for concurrent goroutines

--- main.go
+++ main.go
@@ -8,27 +8,23 @@ import (
 
 func main() {
   tasks := []string{"task1", "task2", "task3", "task4", "task5"}
-  numWorkers := 3
 
-  tasksCh := make(chan string)
+  concurrencyLimit := 2
+  sem := make(chan struct{}, concurrencyLimit)
 
   var wg sync.WaitGroup
-  for i := 0; i < numWorkers; i++ {
-    wg.Add(1)
-    go func() {
-      defer wg.Done()
-      for task := range tasksCh {
-        // Perform task logic here
-        fmt.Println("Processing:", task)
-        time.Sleep(1 * time.Second)
-      }
-    }()
-  }
-
   for _, task := range tasks {
-    tasksCh <- task
+    wg.Add(1)
+    go func(task string) {
+      defer wg.Done()
+      sem <- struct{}{}
+      defer func() { <-sem }()
+
+      // Perform task logic here
+      fmt.Println("Processing: ", task)
+      time.Sleep(1 * time.Second)
+    }(task)
   }
-  close(tasksCh)
 
   wg.Wait()
 }
